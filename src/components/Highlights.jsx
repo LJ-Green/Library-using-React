30497120973c758a34fe87a753dc938e768f0d72
LJ <ljdgreen@live.co.uk>
2023-05-25 20:39:19 +0100

    React Development

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,36 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import Highlight from "./ui/Highlight";
+
+const Highlights = () => {
+  return (
+    <section id="highlights">
+      <div className="container">
+        <div className="row">
+          <h2 className="section__title">
+            Why choose <span className="purple">Library</span>
+          </h2>
+          <div className="highlight__wrapper">
+            <Highlight
+              icon={<FontAwesomeIcon icon="fa-solid fa-bolt" />}
+              title="Easy and Quick"
+              para="Get access to the book you purchased online instantly."
+            />
+            <Highlight
+              icon={<FontAwesomeIcon icon="fa-solid fa-book-open" />}
+              title="10,000+ Books"
+              para="Library has books from all your favourite categories."
+            />
+            <Highlight
+              icon={<FontAwesomeIcon icon="fa-solid fa-tags" />}
+              title="Affordable"
+              para="Get your hands on popular books for as little as £10."
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Highlights;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars, faCartShopping, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfStroke, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+library.add(faBars, faCartShopping, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfStroke, faArrowLeft)
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
